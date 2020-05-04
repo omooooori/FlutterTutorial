9e9b261dca552ce1efdfd1ae66e64f84e8269886
ora1027 <r.shimizu18020@gmail.com>
2020-05-04 16:02:18 +0900

    #3 Remove not needed code.

--- flutter_animation_app/lib/main.dart
+++ flutter_animation_app/lib/main.dart
@@ -17,38 +17,13 @@ class _LogoAppState
   @override initState() {
     super.initState();
     controller = AnimationController(duration: const Duration(seconds: 2), vsync: this);
-//    animation = Tween<double>(begin: 0, end: 300).animate(controller)
-//    ..addListener(() {
-//      setState(() {
-//
-//      });
-//    });
-
     animation = Tween<double>(begin: 0, end: 300).animate(controller)
-      ..addStatusListener((status) {
-        if (status == AnimationStatus.completed) {
-          controller.reverse();
-        } else if (status == AnimationStatus.dismissed) {
-          controller.forward();
-        }
-      })
       ..addStatusListener((state) => print('$state'));
 
     controller.forward();
   }
 
   @override
-//  Widget build(BuildContext context) {
-//    return Center(
-//      child: Container(
-//        margin: EdgeInsets.symmetric(vertical: 10),
-//        height: animation.value,
-//        width: animation.value,
-//        child: FlutterLogo(),
-//      ),
-//    );
-//  }
-//  Widget build(BuildContext context) => AnimatedLogo(animation: animation);
   Widget build(BuildContext context) => GrowTransition(
     child: LogoWidget(),
     animation: animation,
